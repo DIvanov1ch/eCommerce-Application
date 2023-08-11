e0a97fc4e147d94fe2f57545c085a6d84be59911
beskibeski <pypsik312@gmail.com>
2023-08-11 22:22:10 +0300

    feat: add email validation and checkbox for password visibility

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,12 +18,19 @@ export default class Login {
     loginFormHeading.innerText = 'Login';
     const inputUserEmail = document.createElement('input');
     inputUserEmail.classList.add('login__user-email');
-    inputUserEmail.setAttribute('type', 'email');
     inputUserEmail.setAttribute('placeholder', 'User email');
     const inputUserPassword = document.createElement('input');
     inputUserPassword.setAttribute('type', 'password');
     inputUserPassword.setAttribute('placeholder', 'User password');
     inputUserPassword.classList.add('login__user-password');
+    const showPasswordBlock = document.createElement('div');
+    showPasswordBlock.classList.add('login__password-check-box-block');
+    const inputShowOrHidePasswordLabel = document.createElement('label');
+    inputShowOrHidePasswordLabel.classList.add('login__password-check-box-label');
+    inputShowOrHidePasswordLabel.innerText = 'Show password';
+    const inputShowOrHidePasswordCheckBox = document.createElement('input');
+    inputShowOrHidePasswordCheckBox.setAttribute('type', 'checkbox');
+    inputShowOrHidePasswordCheckBox.classList.add('login__password-check-box');
     const inputLoginFormSubmit = document.createElement('input');
     inputLoginFormSubmit.setAttribute('type', 'button');
     inputLoginFormSubmit.classList.add('login__button');
@@ -32,6 +39,9 @@ export default class Login {
     loginForm.append(loginFormHeading);
     loginForm.append(inputUserEmail);
     loginForm.append(inputUserPassword);
+    loginForm.append(showPasswordBlock);
+    showPasswordBlock.append(inputShowOrHidePasswordLabel);
+    showPasswordBlock.append(inputShowOrHidePasswordCheckBox);
     loginForm.append(inputLoginFormSubmit);
   };
 
@@ -86,7 +96,7 @@ export default class Login {
     }
   };
 
-  private static checkPasswordForWhiteSpaces = (element: HTMLInputElement): void => {
+  private static checkInputForWhiteSpaces = (element: HTMLInputElement): void => {
     if (!element.value.match(/^\s+|\s+$/g)) {
       element.classList.add('correct');
       element.classList.remove('incorrect');
@@ -99,7 +109,6 @@ export default class Login {
   private static checkPasswordValidation = (): void => {
     const inputUserPassword = document.querySelector('.login__user-password') as HTMLInputElement;
     inputUserPassword.addEventListener('input', (): void => {
-      console.log(inputUserPassword.value);
       this.checkPasswordLength(inputUserPassword);
       if (this.hasCorrectLength === true) {
         this.checkPasswordForOneUpperOrLowerCaseLetter(inputUserPassword);
@@ -116,7 +125,48 @@ export default class Login {
         this.hasNumbers &&
         this.hasSpecialSymbols
       ) {
-        this.checkPasswordForWhiteSpaces(inputUserPassword);
+        this.checkInputForWhiteSpaces(inputUserPassword);
+      }
+    });
+  };
+
+  private static checkEmailForEmailSymbolAndDomains = (element: HTMLInputElement): void => {
+    if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(element.value)) {
+      element.classList.add('correct');
+      element.classList.remove('incorrect');
+    } else {
+      element.classList.remove('correct');
+      element.classList.add('incorrect');
+    }
+  };
+
+  private static checkEmailValidation = (): void => {
+    const inputUserEmail = document.querySelector('.login__user-email') as HTMLInputElement;
+    inputUserEmail.addEventListener('input', (): void => {
+      if (inputUserEmail.value.length > 0) {
+        this.checkInputForWhiteSpaces(inputUserEmail);
+        if (inputUserEmail.classList.contains('correct')) {
+          this.checkEmailForEmailSymbolAndDomains(inputUserEmail);
+        }
+      } else {
+        inputUserEmail.classList.remove('correct');
+        inputUserEmail.classList.remove('incorrect');
+      }
+    });
+  };
+
+  private static showOrHidePassword = (): void => {
+    const inputShowOrHidePassword = document.querySelector('.login__password-check-box') as HTMLInputElement;
+    const inputUserPassword = document.querySelector('.login__user-password') as HTMLInputElement;
+    const inputShowOrHidePasswordLabel = document.querySelector('.login__password-check-box-label') as HTMLElement;
+    inputShowOrHidePassword.addEventListener('click', (): void => {
+      if (inputUserPassword.getAttribute('type') === 'password') {
+        console.log(inputShowOrHidePasswordLabel);
+        inputUserPassword.setAttribute('type', 'text');
+        inputShowOrHidePasswordLabel.innerText = 'Hide password';
+      } else {
+        inputUserPassword.setAttribute('type', 'password');
+        inputShowOrHidePasswordLabel.innerText = 'Show password';
       }
     });
   };
@@ -124,5 +174,7 @@ export default class Login {
   public static create = (): void => {
     this.createHTMLCode();
     this.checkPasswordValidation();
+    this.checkEmailValidation();
+    this.showOrHidePassword();
   };
 }
