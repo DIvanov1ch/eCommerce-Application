8a6c663a9262772f9430270b1887d52fc978557a
beskibeski <pypsik312@gmail.com>
2023-08-13 23:51:32 +0300

    fix: add . to email pattern

--- src/constants/pattern.ts
+++ src/constants/pattern.ts
@@ -1,3 +1,3 @@
-const emailPattern = /^[a-z0-9!#$%&'*+/=?^_‘{|}~-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+const emailPattern = /^[a-z0-9.!#$%&'*+/=?^_‘{|}~-]+@[a-z0-9.-]+\.[a-z]{2,}$/;
 
 export default emailPattern;
