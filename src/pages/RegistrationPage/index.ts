051dd9b0c8d801a6470a41b7b68349bb74260f6b
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2023-08-28 21:02:55 +0300

    refactor: remove redundant date field classes

--- src/pages/RegistrationPage/index.ts
+++ src/pages/RegistrationPage/index.ts
@@ -76,17 +76,17 @@ export default class RegistrationPage extends Page {
 
     const countryFileds: NodeListOf<HTMLInputElement> = this.querySelectorAll('[name="country"]');
     countryFileds.forEach((field) => {
-      field.addEventListener('focus', this.showCountrySelect.bind(this));
-      field.addEventListener('input', this.showCountrySelect.bind(this));
+      field.addEventListener('focus', this.showCountryList.bind(this));
+      field.addEventListener('input', this.showCountryList.bind(this));
     });
 
     const countrySelects: NodeListOf<HTMLDivElement> = this.querySelectorAll(`.${CssClasses.SELECT}`);
-    countrySelects.forEach((select) => select.addEventListener('click', this.hideCountrySelect.bind(this)));
+    countrySelects.forEach((select) => select.addEventListener('click', this.hideCountryList.bind(this)));
 
     this.addEventListener('click', this.hidePopupAndRedirect.bind(this));
   }
 
-  private showCountrySelect(event: Event): void {
+  private showCountryList(event: Event): void {
     const target = event.target as HTMLInputElement;
     if (!target.value || target.value.trim() !== 'United States') {
       const selector = target.id;
@@ -98,7 +98,7 @@ export default class RegistrationPage extends Page {
     }
   }
 
-  private hideCountrySelect(event: Event): void {
+  private hideCountryList(event: Event): void {
     const target = event.target as HTMLDivElement;
     const selector = Object.values(InputID).find((id) => target.classList.contains(id));
     const field: HTMLInputElement | null = this.querySelector(`#${selector}`);
@@ -234,7 +234,7 @@ export default class RegistrationPage extends Page {
     this.customer.billingAddresses = [indexOfBilling];
   }
 
-  private registrate(): void {
+  private register(): void {
     registration(this.customer)
       .then((response) => {
         this.isSignUp = true;
@@ -257,7 +257,7 @@ export default class RegistrationPage extends Page {
     if (isValid) {
       this.disableButtons();
       this.setCustomerInformation();
-      this.registrate();
+      this.register();
     } else {
       this.setErrorMessages();
       this.showErrors();
