a6cb431ec0d14f4e0bca98f85282cadc30b8fa0f
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2023-08-31 01:39:50 +0300

    feat: implement edit mode

--- src/pages/UserProfilePage/css-classes.ts
+++ src/pages/UserProfilePage/css-classes.ts
@@ -1,5 +1,4 @@
 enum CssClasses {
-  TABLE = 'user-profile__table',
   BODY = 'user-profile__table-body',
   ROW = 'row-module',
   TYPE_OF_ADDRESS = 'address-type',
@@ -8,6 +7,22 @@ enum CssClasses {
   DEFAULT_SHIPPING = 'default-shipping',
   DEFAULT_BILLING = 'default-billing',
   HIDDEN = 'hidden',
+  ICON_BOX = 'user-profile__icon-box',
+  ICON = 'user-profile__icon',
+  CONTAINER = 'user-profile__container',
+  WRAPPER_WRITE = 'user-profile__icon-wrapper_write',
+  WRAPPER_CANCEL = 'user-profile__icon-wrapper_cancel',
+  EDIT_MODE = 'edit-mode',
+  CELL = 'info__cell',
+  OVERLAY = 'user-profile__overlay',
+  MODAL = 'user-profile__modal-main',
+  NAME = 'container_name',
+  PASSWORD = 'container_password',
+  ADDRESS = 'container_address',
+  ADD_BUTTON = 'user-profile__button-box',
+  SUBMIT_BUTTON = 'submit-button',
+  EDIT_MODE_FLAG = 'addresses__info',
+  FIELD = 'input-module__input-field',
 }
 
 export default CssClasses;
