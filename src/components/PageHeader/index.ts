2713413e588a525ec6a90416a7420c76d4cecaf1
Vitaly Kukushkin <phoinixru@gmail.com>
2023-09-01 18:06:29 +0200

    feat: add catalog link to header

--- src/components/PageHeader/index.ts
+++ src/components/PageHeader/index.ts
@@ -2,13 +2,31 @@ import './header.scss';
 import html from './header.html';
 import BaseComponent from '../BaseComponent';
 
-export default class PageFooter extends BaseComponent {
+const CssClasses = {
+  TOGGLE: 'nav-toggle',
+  OPEN: 'nav--open',
+  CONTAINER: 'nav-container',
+};
+
+export const toggleNavigation = (toggle?: boolean): void => {
+  document.body.classList.toggle(CssClasses.OPEN, toggle);
+};
+
+export default class PageHeader extends BaseComponent {
   constructor() {
     super(html);
   }
 
   protected connectedCallback(): void {
     super.connectedCallback();
-    this.classList.add('footer');
+    this.classList.add('header');
+
+    this.$(`.${CssClasses.TOGGLE}`)?.addEventListener('click', () => toggleNavigation());
+    this.$(`.${CssClasses.CONTAINER}`)?.addEventListener('click', (event) => {
+      const { target } = event;
+      if (target instanceof HTMLAnchorElement) {
+        toggleNavigation();
+      }
+    });
   }
 }
