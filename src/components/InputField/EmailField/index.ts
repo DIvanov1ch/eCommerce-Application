bc5891a86308391fcba8cf6d08bbda6808670d1c
Denis Trukhanovich <denis.trukhanovich@gmail.com>
2023-09-20 13:31:01 +0300

    feat: add EmailField component

---
+++ src/components/InputField/EmailField/index.ts
@@ -0,0 +1,29 @@
+import InputField from '..';
+import Pattern from '../../../constants/pattern';
+import { WarningMessage } from '../../../interfaces';
+import { InputParams } from '../../../types';
+
+const inputParams: InputParams = {
+  id: 'email',
+  type: 'email',
+};
+const labelText = 'Email';
+
+const ErrorMessage: WarningMessage = {
+  emptyField: 'Put your email',
+  invalidValue: 'Wrong or Invalid email address. Please correct it.',
+};
+
+export default class EmailField extends InputField {
+  constructor() {
+    super({ inputParams, labelText });
+  }
+
+  protected isValidValue(): boolean {
+    return Pattern.email.test(this.getInputValue());
+  }
+
+  protected setWarning(message?: WarningMessage): void {
+    super.setWarning(ErrorMessage || message, this);
+  }
+}
